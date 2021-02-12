49f68f31a17f40401583af188e9629c15415414c
Aidan-B1409 <aidanb1049@gmail.com>
2021-02-11 20:31:34 -0800

    security tests for resource categories

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+    # TODO: useless test
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
 end
