89096ac75d169d08f7bdf31cb90276d1db6df25d
Aidan-B1409 <aidanb1049@gmail.com>
2021-02-25 19:50:37 -0800

    ticket controller tests

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,24 +3,68 @@ require 'rails_helper'
 RSpec.describe TicketsController, type: :controller do
   context 'unauthenticated users' do
     before do
+      reg_usr = build(:user)
+      allow(controller).to receive(:current_user).and_return(reg_usr)
+    end
+    it 'redirects post requests to capture to sign_in' do
+      post :capture, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects post requests to release to sign_in' do
+      post :release, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects patch requests to close to sign_in' do
+      patch :close, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects get requests to show to sign_in' do
+      get :show, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects delete requests to destroy to the dashboard' do
+      delete :destroy, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
+  context 'logged-in users' do
+    before do
+      reg_usr = build(:user)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(reg_usr)
+      allow(controller).to receive(:current_user).and_return(reg_usr)
+    end
+    it 'redirects post requests to capture to the dashboard' do
+      post :capture, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects post requests to release to the dashboard' do
+      post :release, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects patch requests to close to the dashboard' do
+      patch :close, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects get requests to show to the dashboard' do
+      get :show, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+    it 'redirects delete requests to destroy to dashboard' do
+      delete :destroy, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
+  context 'organization users' do
+    before do
       org_usr = build(:user, :organization)
       allow(request.env['warden']).to receive(:authenticate!).and_return(org_usr)
       allow(controller).to receive(:current_user).and_return(org_usr)
     end
-    # it 'allows post requests to create' do
-    #   post :create
-    #   expect(response).to be_successful
-    # end
-    # it 'allows get requests to new' do
-    #   get :new
-    #   expect(response).to be_successful
-    # end
-    # it 'redirects get requests for show to sign_in' do
-    #   get :show, params: { id: 'fake' }
-    #   expect(response).to redirect_to(new_user_session_url)
-    # end
-
-    #TODO - more ticket tests
+    it 'redirects delete requests to destroy to dashboard' do
+      delete :destroy, params: { id: 'fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
   end
-
 end
